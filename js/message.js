8c699d2b6c78adaeb434e6aee579d22494c75a87
ArtemSt <129779631+milione7777@users.noreply.github.com>
2024-06-30 15:21:37 +0300

    cleaned up excess

--- js/message.js
+++ js/message.js
@@ -42,11 +42,3 @@ let notificationCount = 0;
                 }, 1500);
             });
         });
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-              document.getElementById("navbar").style.top = "0";
-            } else {
-              document.getElementById("navbar").style.top = "-50px";
-            }
-          }
